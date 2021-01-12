b1e60ea83a4064b32144b39cd67de3c067d43323
A. Diamond <diamond@sheprador.com>
2021-01-12 13:24:40 -0500

    Removed app folder. Moved common to top level

--- common/config_test.go
+++ common/config_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/APTrust/registry/app/common"
+	"github.com/APTrust/registry/common"
 	"github.com/op/go-logging"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
--- common/util_test.go
+++ common/util_test.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/APTrust/registry/app/common"
+	"github.com/APTrust/registry/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
