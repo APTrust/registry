155206ec13608eeea3dfd732e14891088d831df0
A. Diamond <diamond@sheprador.com>
2021-02-15 09:35:40 -0500

    Working on DataStore

--- models/data_store.go
+++ models/data_store.go
@@ -9,11 +9,28 @@ import (
 	"github.com/go-pg/pg/v10"
 )
 
+// DataStore provides methods for storing and retrieving data from our
+// Postgres database. In addition to basic storage and retrieval, this class
+// enforces some business rules, including the following:
+//
+// 1. Restricts access to data that the acting user is not allowed to see.
+//
+// 2. Restricts operations that the acting user is not allowed to peform,
+//    such as updating or deleting records that belong to someone else.
+//
+// 3. Restricts operations that no one (including admin) is allowed to
+//    perform. For example, records such as Checksums and PremisEvents
+//    can never be altered or deleted by anyone.
 type DataStore struct {
 	ctx        *common.APTContext
 	actingUser *User
 }
 
+// NewDataStore creates a new DataStore object for the actingUser. Note
+// that this is a lightweight operation that uses existing DB connections
+// from the connection pool. This class will check the acting user's
+// permissions for all operations, and will not create, update, delete, or
+// return data unless the acting user is authorized to access it.
 func NewDataStore(actingUser *User) *DataStore {
 	return &DataStore{
 		ctx:        common.Context(),
@@ -21,16 +38,210 @@ func NewDataStore(actingUser *User) *DataStore {
 	}
 }
 
+// ChecksumFind returns the Checksum with the specified ID.
+func (ds *DataStore) ChecksumFind(id int64) (*Checksum, error) {
+	checksum := &Checksum{}
+	err := ds.find(checksum, id)
+	return checksum, err
+}
+
+// ChecksumList returns a list of checksums matching the query.
+func (ds *DataStore) ChecksumList(q *Query) ([]*Checksum, error) {
+	checksums := make([]*Checksum, 0)
+	err := ds._select(&checksums, q)
+	return checksums, err
+}
+
+// ChecksumSave saves a new checksum. If you try to update an existing
+// checksum, you'll get an error, because business rules say these
+// records are immutable.
+func (ds *DataStore) ChecksumSave(cs *Checksum) error {
+	return ds.save(cs)
+}
+
+// GenericFileDelete sets the State attribute of the GenericFile to "D".
+// This is a soft delete.
+func (ds *DataStore) GenericFileDelete(gf *GenericFile) error {
+	return ds.delete(gf)
+}
+
+// GenericFileFind returns the GenericFile with the specified ID.
+func (ds *DataStore) GenericFileFind(id int64) (*GenericFile, error) {
+	gf := &GenericFile{}
+	err := ds.find(gf, id)
+	return gf, err
+}
+
+// GenericFileFindByIdentifier returns the GenericFile with the specified
+// identifier.
+func (ds *DataStore) GenericFileFindByIdentifier(identifier string) (*GenericFile, error) {
+	gf := &GenericFile{}
+	query := NewQuery().Where("identifier", "=", identifier).Limit(1)
+	err := ds._select(gf, query)
+	return gf, err
+}
+
+// GenericFileList returns the GenericFiles matching the specified query.
+func (ds *DataStore) GenericFileList(q *Query) ([]*GenericFile, error) {
+	gfs := make([]*GenericFile, 0)
+	err := ds._select(&gfs, q)
+	return gfs, err
+}
+
+// GenericFileSave saves a GenericFile.
+func (ds *DataStore) GenericFileSave(gf *GenericFile) error {
+	return ds.save(gf)
+}
+
+// GenericFileUndelete sets the State attribute of the GenericFile to "A".
+// This undoes the soft delete.
+func (ds *DataStore) GenericFileUnelete(gf *GenericFile) error {
+	return ds.undelete(gf)
+}
+
+// InstitutionDelete sets the DeactivatedAt timestamp on an Institution,
+// marking it as no longer active. This is a soft delete.
+func (ds *DataStore) InstitutionDelete(inst *Institution) error {
+	return ds.delete(inst)
+}
+
+// InstitutionFind returns the Institution with the specified ID.
+func (ds *DataStore) InstitutionFind(id int64) (*Institution, error) {
+	inst := &Institution{}
+	err := ds.find(inst, id)
+	return inst, err
+}
+
+// InstitutionFindByIdentifier returns the Institution with the specified
+// identifier.
+func (ds *DataStore) InstitutionFindByIdentifier(identifier string) (*Institution, error) {
+	inst := &Institution{}
+	query := NewQuery().Where("identifier", "=", identifier).Limit(1)
+	err := ds._select(inst, query)
+	return inst, err
+}
+
+// InstitutionList returns the Institutions matching the query.
 func (ds *DataStore) InstitutionList(q *Query) ([]*Institution, error) {
 	institutions := make([]*Institution, 0)
 	err := ds._select(&institutions, q)
 	return institutions, err
 }
 
+// InstitutionSave saves an Institution record.
+func (ds *DataStore) InstitutionSave(inst *Institution) error {
+	return ds.save(inst)
+}
+
+// InstitutionUnelete clears the DeactivatedAt timestamp on an Institution,
+// marking it as once again active. This undoes soft delete.
+func (ds *DataStore) InstitutionUndelete(inst *Institution) error {
+	return ds.undelete(inst)
+}
+
+// IntellectualObjectDelete marks an object as deleted by setting its
+// State to "D". This is a soft delete.
+func (ds *DataStore) IntellectualObjectDelete(obj *IntellectualObject) error {
+	return ds.delete(obj)
+}
+
+// IntellectualObjectFind returns the IntellectualObject with the specified
+// ID.
+func (ds *DataStore) IntellectualObjectFind(id int64) (*IntellectualObject, error) {
+	obj := &IntellectualObject{}
+	err := ds.find(obj, id)
+	return obj, err
+}
+
+// IntellectualObjectFindByIdentifier returns the IntellectualObject
+// with the specified identifier.
+func (ds *DataStore) IntellectualObjectFindByIdentifier(identifier string) (*IntellectualObject, error) {
+	obj := &IntellectualObject{}
+	query := NewQuery().Where("identifier", "=", identifier).Limit(1)
+	err := ds._select(obj, query)
+	return obj, err
+}
+
+// IntellectualObjectList returns IntellectualObjects that match the query.
+func (ds *DataStore) IntellectualObjectList(q *Query) ([]*IntellectualObject, error) {
+	objitutions := make([]*IntellectualObject, 0)
+	err := ds._select(&objitutions, q)
+	return objitutions, err
+}
+
+// IntellectualObjectSave inserts or updates an IntellectualObject.
+func (ds *DataStore) IntellectualObjectSave(obj *IntellectualObject) error {
+	return ds.save(obj)
+}
+
+// IntellectualObjectUndelete marks an object as active by setting its
+// State to "A". This undoes soft delete.
+func (ds *DataStore) IntellectualObjectUndelete(obj *IntellectualObject) error {
+	return ds.undelete(obj)
+}
+
+// PremisEventFind returns the PremisEvent with the specified ID.
+func (ds *DataStore) PremisEventFind(id int64) (*PremisEvent, error) {
+	event := &PremisEvent{}
+	err := ds.find(event, id)
+	return event, err
+}
+
+// PremisEventFindByIdentifier returns the PremisEvent with the specified
+// identifier.
+func (ds *DataStore) PremisEventFindByIdentifier(identifier string) (*PremisEvent, error) {
+	event := &PremisEvent{}
+	query := NewQuery().Where("identifier", "=", identifier).Limit(1)
+	err := ds._select(event, query)
+	return event, err
+}
+
+// PremisEventList returns the PremisEvents that match the query.
+func (ds *DataStore) PremisEventList(q *Query) ([]*PremisEvent, error) {
+	events := make([]*PremisEvent, 0)
+	err := ds._select(&events, q)
+	return events, err
+}
+
+// PremisEventSave saves a new PremisEvent. Attempting to update an existing
+// PremisEvent will cause an error as events are read-only.
+func (ds *DataStore) PremisEventSave(event *PremisEvent) error {
+	return ds.save(event)
+}
+
+// StorageRecordFind returns the StorageRecord with the specified ID.
+func (ds *DataStore) StorageRecordFind(id int64) (*StorageRecord, error) {
+	sr := &StorageRecord{}
+	err := ds.find(sr, id)
+	return sr, err
+}
+
+// StorageRecordList returns a list of StorageRecords matching the query.
+func (ds *DataStore) StorageRecordList(q *Query) ([]*StorageRecord, error) {
+	records := make([]*StorageRecord, 0)
+	err := ds._select(&records, q)
+	return records, err
+}
+
+// StorageRecordSave saves a new StorageRecord or updates an existing one.
+func (ds *DataStore) StorageRecordSave(sr *StorageRecord) error {
+	return ds.save(sr)
+}
+
+// StorageRecordDelete deletes a StorageRecord. Note that this is a hard
+// delete and cannot be undone.
+func (ds *DataStore) StorageRecordDelete(sr *StorageRecord) error {
+	return ds.delete(sr)
+}
+
+// UserDelete sets the DeactivatedAt timestamp on a User to indicate their
+// account is no longer active. This is a soft delete and can be undone later.
 func (ds *DataStore) UserDelete(user *User) error {
 	return ds.delete(user)
 }
 
+// UserFind returns the User with the specified ID. The User record will
+// include the related Instution record.
 func (ds *DataStore) UserFind(id int64) (*User, error) {
 	user := &User{}
 	err := ds.ctx.DB.Model(user).Relation("Institution").Where(`"user"."id" = ?`, id).Select()
@@ -41,6 +252,8 @@ func (ds *DataStore) UserFind(id int64) (*User, error) {
 	return user, err
 }
 
+// UserFindByEmail returns the User with the specified email address.
+// The User record will include the related Instution record.
 func (ds *DataStore) UserFindByEmail(email string) (*User, error) {
 	user := &User{}
 	err := ds.ctx.DB.Model(user).Relation("Institution").Where(`"user"."email" = ?`, email).Select()
@@ -51,16 +264,21 @@ func (ds *DataStore) UserFindByEmail(email string) (*User, error) {
 	return user, err
 }
 
+// UserList returns a list of Users matching the specified query.
 func (ds *DataStore) UserList(q *Query) ([]*User, error) {
 	users := make([]*User, 0)
 	err := ds._select(&users, q)
 	return users, err
 }
 
+// UserSave inserts or updates a User record.
 func (ds *DataStore) UserSave(user *User) error {
 	return ds.save(user)
 }
 
+// UserSignIn signs a user in. If successful, it returns the User
+// record with User.Institution properly set. If it fails, check
+// the error.
 func (ds *DataStore) UserSignIn(email, password, ipAddr string) (*User, error) {
 	user, err := ds.UserFindByEmail(email)
 	if IsNoRowError(err) {
@@ -88,6 +306,7 @@ func (ds *DataStore) UserSignIn(email, password, ipAddr string) (*User, error) {
 	return user, err
 }
 
+// UserSignOut signs a user out.
 func (ds *DataStore) UserSignOut(user *User) error {
 	if user.CurrentSignInIP != "" {
 		user.LastSignInIP = user.CurrentSignInIP
@@ -100,14 +319,36 @@ func (ds *DataStore) UserSignOut(user *User) error {
 	return ds.save(user)
 }
 
+// UserUndelete cleans the DeactivatedAt timestamp on a user record to
+// indicate that their account is active.
 func (ds *DataStore) UserUndelete(user *User) error {
 	return ds.undelete(user)
 }
 
+// UserViewList returns a list of UserView objects.
 func (ds *DataStore) UserViewList(q *Query) ([]*UserView, error) {
 	records := make([]*UserView, 0)
 	err := ds._select(&records, q)
 	return records, err
+}
+
+// WorkItemFind returns the WorkItem with the specified ID.
+func (ds *DataStore) WorkItemFind(id int64) (*WorkItem, error) {
+	item := &WorkItem{}
+	err := ds.find(item, id)
+	return item, err
+}
+
+// WorkItemList returns a list of WorkItems matching the query.
+func (ds *DataStore) WorkItemList(q *Query) ([]*WorkItem, error) {
+	items := make([]*WorkItem, 0)
+	err := ds._select(&items, q)
+	return items, err
+}
+
+// WorkItemSave inserts or updates an WorkItem.
+func (ds *DataStore) WorkItemSave(item *WorkItem) error {
+	return ds.save(item)
 }
 
 // Private
