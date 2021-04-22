50d60147edb06034079da8d990f294ce08c47ea6
A. Diamond <diamond@sheprador.com>
2021-04-22 15:16:36 -0400

    Added modal dialog

--- static/js/registry.js
+++ static/js/registry.js
@@ -1,1 +1,28 @@
 // registry.js
+APT = {
+
+    initModals: function() {
+        // Get all buttons/links that have a data-modal attibute
+        var modals = document.querySelectorAll("[data-modal]");
+
+        // Attach callbacks to the modal controllers.
+        modals.forEach(function (modalController) {
+            modalController.addEventListener("click", function (event) {
+                event.preventDefault();
+                var modal = document.getElementById(modalController.dataset.modal);
+                modal.classList.add("open");
+                var exits = modal.querySelectorAll(".modal-exit");
+                exits.forEach(function (exit) {
+                    exit.addEventListener("click", function (event) {
+                        event.preventDefault();
+                        modal.classList.remove("open");
+                    });
+                });
+            });
+        });
+    }
+}
+
+window.addEventListener('load', (event) => {
+    APT.initModals();
+});
