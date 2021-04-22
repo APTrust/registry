fb1cbdb5702bd821b27fccfe6af8aaa8a5a9465c
A. Diamond <diamond@sheprador.com>
2021-04-22 09:20:09 -0400

    Added ObjectStats

---
+++ pgmodels/object_stats_test.go
@@ -0,0 +1,25 @@
+package pgmodels_test
+
+import (
+	"testing"
+
+	"github.com/APTrust/registry/db"
+	"github.com/APTrust/registry/pgmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var expected = []*pgmodels.ObjectStats{
+	{1, 2233889, "application/binary"},
+	{1, 78333, "audio/mp3"},
+	{1, 988226, "text/plain"},
+	{1, 399288, "video/mp4"},
+	{4, 3699736, ""},
+}
+
+func TestGetObjectStats(t *testing.T) {
+	db.LoadFixtures()
+	stats, err := pgmodels.GetObjectStats(3)
+	require.Nil(t, err)
+	assert.Equal(t, expected, stats)
+}
