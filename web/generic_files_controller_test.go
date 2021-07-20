cf074ab92f39048ceb1c1c55816a26cc2ead4cd5
A. Diamond <diamond@sheprador.com>
2021-07-20 16:13:15 -0400

    Test GenericFile RequestDelete

--- web/generic_files_controller_test.go
+++ web/generic_files_controller_test.go
@@ -3,7 +3,10 @@ package web_test
 import (
 	"net/http"
 	"testing"
-	//"github.com/stretchr/testify/require"
+
+	"github.com/APTrust/registry/pgmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenericFileShow(t *testing.T) {
@@ -121,13 +124,53 @@ func TestGenericFileRequestDelete(t *testing.T) {
 	// Sys Admin can request any deletion, but others cannot
 	// request deletions outside their own institution.
 	// File 18 from fixtures belongs to Inst2
-	sysAdminClient.GET("/files/request_delete/18").Expect().Status(http.StatusOK)
-	instAdminClient.GET("/files/request_delete/18").Expect().Status(http.StatusForbidden)
-	instUserClient.GET("/files/request_delete/18").Expect().Status(http.StatusForbidden)
+	sysAdminClient.GET("/files/request_delete/18").
+		Expect().Status(http.StatusOK)
+	instAdminClient.GET("/files/request_delete/18").
+		Expect().Status(http.StatusForbidden)
+	instUserClient.GET("/files/request_delete/18").
+		Expect().Status(http.StatusForbidden)
 }
 
 func TestGenericFileInitDelete(t *testing.T) {
 	initHTTPTests(t)
+
+	items := []string{
+		"Deletion Requested",
+		"institution1.edu/pdfs/doc1",
+	}
+
+	// User at inst 1 can initiate deletion of their own
+	// institution's file.
+	html := instUserClient.POST("/files/init_delete/4").Expect().Status(http.StatusCreated).Body().Raw()
+	MatchesAll(t, html, items)
+
+	// This should create a deletion request...
+	query := pgmodels.NewQuery().Where("generic_file_id", "=", 4)
+	drgf := pgmodels.DeletionRequestsGenericFiles{}
+	err := query.Select(&drgf)
+	require.Nil(t, err)
+	require.NotEqual(t, int64(0), drgf.DeletionRequestID)
+
+	deletionRequest, err := pgmodels.DeletionRequestByID(drgf.DeletionRequestID)
+	require.Nil(t, err)
+	require.NotNil(t, deletionRequest)
+
+	// Make sure this is the request our test user just made
+	require.Equal(t, inst1User.ID, deletionRequest.RequestedByID)
+
+	// There should also be an alert...
+	query = pgmodels.NewQuery().Where("deletion_request_id", "=", drgf.DeletionRequestID).Relations("DeletionRequest", "Users")
+	alert, err := pgmodels.AlertGet(query)
+	require.Nil(t, err)
+	assert.NotNil(t, alert)
+	assert.NotNil(t, alert.DeletionRequest)
+	assert.True(t, len(alert.Users) > 0)
+
+	// The user should NOT be able to initiate deletion of a file
+	// that belongs to another institution. In fixture data, file
+	// 34 belongs to inst 2.
+	instUserClient.POST("/files/init_delete/34").Expect().Status(http.StatusForbidden)
 }
 
 func TestGenericFileRequestRestore(t *testing.T) {
