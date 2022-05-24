d0b817f48a326317d620edc508d8e44ecdb25f16
A. Diamond <diamond@sheprador.com>
2022-05-24 14:07:56 -0400

    Working on email client

---
+++ network/ses_client.go
@@ -0,0 +1,82 @@
+package network
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/rs/zerolog"
+)
+
+type SESClient struct {
+	logger         zerolog.Logger
+	FromAddress    string
+	ServiceEnabled bool
+	Session        *session.Session
+	Service        *ses.SES
+}
+
+func NewSESClient(serviceEnabled bool, awsRegion, fromAddress string, logger zerolog.Logger) *SESClient {
+	client := &SESClient{
+		logger:         logger,
+		ServiceEnabled: serviceEnabled,
+	}
+	if serviceEnabled {
+		client.Session = session.Must(session.NewSession(&aws.Config{
+			Region: aws.String(awsRegion)},
+		))
+		client.Service = ses.New(client.Session)
+		logger.Info().Msg("Email service is enabled. Alerts will be sent through AWS SES service.")
+	} else {
+		logger.Info().Msg("Email service is disabled. Alerts will be written to the log file.")
+	}
+	return client
+}
+
+// Send sends an email to the specified address.
+func (client *SESClient) Send(emailAddress, subject, message string) error {
+	if client.ServiceEnabled {
+		return client.sendRealEmail(emailAddress, subject, message)
+	} else {
+		return client.sendDummyEmail(emailAddress, subject, message)
+	}
+}
+
+func (client *SESClient) sendRealEmail(emailAddress, subject, message string) error {
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: []*string{
+				aws.String(emailAddress),
+			},
+		},
+		Message: &ses.Message{
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
+				Data:    aws.String(subject),
+			},
+			Body: &ses.Body{
+				Text: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String(message),
+				},
+			},
+		},
+		ReturnPath: aws.String(client.FromAddress),
+		Source:     aws.String(client.FromAddress),
+	}
+	output, err := client.Service.SendEmail(input)
+	msg := fmt.Sprintf("SMS to %s: %s", emailAddress, output.String())
+	if err == nil {
+		client.logger.Info().Msg(msg)
+	} else {
+		client.logger.Error().Msgf("%s (%s)", msg, err.Error())
+	}
+	return err
+}
+
+func (client *SESClient) sendDummyEmail(emailAddress, subject, message string) error {
+	msg := fmt.Sprintf("SES is disabled per config settings. Email to %s. Subject: %s\n\n. %s", emailAddress, subject, message)
+	client.logger.Info().Msgf(msg)
+	return nil
+}
