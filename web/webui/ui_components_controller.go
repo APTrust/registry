f5137a9f267004cadff45d2051e1ec619f5a65ce
mhodges-st <marcus.hodges@simplethread.com>
2022-05-24 11:59:27 -0400

    Suppressed TopNav on ui_components controller

--- web/webui/ui_components_controller.go
+++ web/webui/ui_components_controller.go
@@ -10,6 +10,7 @@ import (
 func ComponentsIndex(c *gin.Context) {
 	templateData := gin.H{
 		"suppressSideNav": true,
+		"suppressTopNav":  true,
 	}
 	c.HTML(http.StatusOK, "ui_components/index.html", templateData)
 }
