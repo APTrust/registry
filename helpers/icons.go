57f15ed042ddafa25f6201fa5526af364db567c0
mhodges-st <marcus.hodges@simplethread.com>
2022-06-16 13:16:39 -0400

    Updated events list
    TODO Fix pager

--- helpers/icons.go
+++ helpers/icons.go
@@ -5,22 +5,22 @@ import (
 )
 
 // IconMissing is the icon we show for items that are not in the IconMap.
-// This appears as an upside-down question mark. If you see it on any web
+// This appears as a question mark. If you see it on any web
 // page, you should know to add an appropriate icon to the IconMap.
-var IconMissing = `<i class="far fa-question-circle fa-rotate-180"></i>`
+var IconMissing = `help_outline`
 
-// IconMap maps strings to FontAwesome icons.
+// IconMap maps strings to Material icons.
 var IconMap = map[string]string{
 
 	// Premis Event Icons.
 	// Note that we use only 7 or so event types,
 	// so we don't define an icon for every type.
-	constants.EventAccessAssignment:     `<i class="fas fa-user-shield"></i>`,
-	constants.EventCreate:               `<i class="fas fa-plus-circle"></i>`,
-	constants.EventDeletion:             `<i class="fas fa-minus-circle"></i>`,
-	constants.EventDigestCalculation:    `<i class="fas fa-file-signature"></i>`,
-	constants.EventFixityCheck:          `<i class="fas fa-fingerprint"></i>`,
-	constants.EventIdentifierAssignment: `<i class="far fa-snowflake"></i>`,
-	constants.EventIngestion:            `<i class="fas fa-file-import"></i>`,
-	constants.EventReplication:          `<i class="fas fa-copy"></i>`,
+	constants.EventAccessAssignment:     `admin_panel_settings`,
+	constants.EventCreate:               `add_circle_outline`,
+	constants.EventDeletion:             `delete_forever`,
+	constants.EventDigestCalculation:    `description`,
+	constants.EventFixityCheck:          `fingerprint`,
+	constants.EventIdentifierAssignment: `search`,
+	constants.EventIngestion:            `file_upload`,
+	constants.EventReplication:          `library_books`,
 }
