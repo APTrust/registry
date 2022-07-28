fcb2e222c385bdf121916cf6bae50a722f896623
A. Diamond <diamond@sheprador.com>
2022-07-28 14:07:38 -0400

    Updated helpers and added examples to set icon and badge class. https://trello.com/c/U8SnKjxU

--- helpers/icons.go
+++ helpers/icons.go
@@ -15,14 +15,21 @@ var IconMap = map[string]string{
 	// Premis Event Icons.
 	// Note that we use only 7 or so event types,
 	// so we don't define an icon for every type.
-	constants.EventAccessAssignment:     `admin_panel_settings`,
-	constants.EventCreate:               `add_circle_outline`,
-	constants.EventDeletion:             `delete_forever`,
-	constants.EventDigestCalculation:    `description`,
-	constants.EventFixityCheck:          `fingerprint`,
-	constants.EventIdentifierAssignment: `search`,
-	constants.EventIngestion:            `file_upload`,
-	constants.EventReplication:          `library_books`,
+	constants.EventAccessAssignment:     "admin_panel_settings",
+	constants.EventCreate:               "add_circle_outline",
+	constants.EventDeletion:             "delete_forever",
+	constants.EventDigestCalculation:    "description",
+	constants.EventFixityCheck:          "fingerprint",
+	constants.EventIdentifierAssignment: "search",
+	constants.EventIngestion:            "file_upload",
+	constants.EventReplication:          "library_books",
+
+	// WorkItem actions
+	constants.ActionDelete:         "delete_forever",
+	constants.ActionIngest:         "file_upload",
+	constants.ActionRestoreFile:    "file_download",
+	constants.ActionRestoreObject:  "file_download",
+	constants.ActionGlacierRestore: "file_download",
 }
 
 // BadgeClassMap maps work item status and other constant values
