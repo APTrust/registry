6101ae5527833d45dbfd1eb1582c1e0b62740831
A. Diamond <diamond@sheprador.com>
2024-03-18 15:32:01 -0400

    Removed extra opening bracket. Fixed initial display of subnav based on .openSubMenu.

--- static/js/modules/sidebar.js
+++ static/js/modules/sidebar.js
@@ -15,5 +15,20 @@ export function initSidebar() {
         sidebarSubnav.classList.toggle('is-sr-only');
         sidebarMore.classList.toggle('is-hidden');
         sidebarLess.classList.toggle('is-hidden');
+
+        // The following is for aria keyboard navigation. We don't want
+        // assistive technologies to tab into a hidden sub-menu. Setting
+        // both aria-expanded and display=block/hidden gives us the 
+        // correct behavior. Tabbing skips closed menu items but works
+        // on open ones. Part of the general accessibility card at
+        // https://trello.com/c/CEQ5jAe1
+        var subnavIsOpen = sidebarMore.classList.contains("is-hidden")
+        if (subnavIsOpen) {
+            sidebarToggle.setAttribute("aria-expanded", "true")
+            sidebarSubnav.style.display = "block"
+        } else {
+            sidebarToggle.setAttribute("aria-expanded", "false")
+            sidebarSubnav.style.display = "none"
+        }
     });
 }
