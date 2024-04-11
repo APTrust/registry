23e80a533de88af3a4c1f39d9f92ec43c6abcf0a
A. Diamond <diamond@sheprador.com>
2024-04-11 11:51:17 -0400

    Reverted last commit

--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -41,17 +41,18 @@ func Authenticate() gin.HandlerFunc {
 				c.Set("CurrentUser", user)
 			}
 		}
-		if forceCompletionOfTwoFactorAuth(c, user) {
-			log2FAIncomplete(c, user)
-			c.Redirect(http.StatusFound, "/users/2fa_choose/")
-			c.Abort()
-		} else if forceCompletionOfPasswordChange(c, user) {
+		if forceCompletionOfPasswordChange(c, user) {
 			logPasswordChangeIncomplete(c, user)
 			c.HTML(http.StatusUnauthorized, "errors/show.html", gin.H{
 				"suppressSideNav": true,
 				"suppressTopNav":  true,
 				"error":           "Please finish changing your password",
 			})
+			c.Abort()
+		}
+		if forceCompletionOfTwoFactorAuth(c, user) {
+			log2FAIncomplete(c, user)
+			c.Redirect(http.StatusFound, "/users/2fa_choose/")
 			c.Abort()
 		}
 		c.Next()
