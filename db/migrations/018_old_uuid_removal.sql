aa90657431ff1f2a419d9f2cc78603c28b59a1f9
Melissa Iori <melissa.iori@aptrust.org>
2026-08-11 15:40:06 -0400

    Removed unneeded reindexing from migration

--- db/migrations/018_old_uuid_removal.sql
+++ db/migrations/018_old_uuid_removal.sql
@@ -7,33 +7,12 @@ insert into schema_migrations ("version", started_at) values ('018_old_uuid_remo
 on conflict ("version") do update set started_at = now();
 
 -- First we have to remove dependent objects temporarily
-drop index index_premis_events_on_event_type;
-drop index index_premis_events_on_event_type_and_outcome;
-drop index ix_premis_event_counts;
-drop materialized view public.premis_event_counts;
 drop view public.premis_events_view;
 
 -- Drop the column.
 alter table premis_events drop column old_uuid;
 
--- Recreate indices and views that use this and reindex - may take some time before indexing is complete
-CREATE INDEX index_premis_events_on_event_type ON public.premis_events USING btree (event_type);
-CREATE INDEX index_premis_events_on_event_type_and_outcome ON public.premis_events USING btree (event_type, outcome);
-
-CREATE MATERIALIZED VIEW public.premis_event_counts
-TABLESPACE pg_default
-AS SELECT premis_events.institution_id,
-    count(premis_events.id) AS row_count,
-    premis_events.event_type,
-    premis_events.outcome,
-    CURRENT_TIMESTAMP AS updated_at
-   FROM premis_events
-  GROUP BY CUBE(premis_events.institution_id, premis_events.event_type, premis_events.outcome)
-  ORDER BY premis_events.institution_id, premis_events.event_type, premis_events.outcome
-WITH DATA;
-
-CREATE UNIQUE INDEX ix_premis_event_counts ON public.premis_event_counts USING btree (institution_id, event_type, outcome);
-
+-- Recreate the Premis Events view
 CREATE OR REPLACE VIEW public.premis_events_view
 AS SELECT pe.id,
     pe.identifier,
