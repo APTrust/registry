877584c820afe2017f376670e897cde9b25913e2
Melissa Iori <melissa.iori@aptrust.org>
2026-08-11 15:45:16 -0400

    Reset migration number

--- db/migrations/015_old_uuid_removal.sql
+++ db/migrations/015_old_uuid_removal.sql
@@ -1,16 +1,16 @@
--- 018_old_uuid_removal.sql
+-- 015_old_uuid_removal.sql
 --
 -- Removes unneeded old_uuid column from PremisEvents.
 
 -- Note that we're starting the migration.
-insert into schema_migrations ("version", started_at) values ('018_old_uuid_removal', now())
+insert into schema_migrations ("version", started_at) values ('015_old_uuid_removal', now())
 on conflict ("version") do update set started_at = now();
 
 -- First we have to remove dependent objects temporarily
 drop view public.premis_events_view;
 
 -- Drop the column.
-alter table premis_events drop column old_uuid;
+alter table premis_events drop column if exists old_uuid;
 
 -- Recreate the Premis Events view
 CREATE OR REPLACE VIEW public.premis_events_view
@@ -38,4 +38,4 @@ AS SELECT pe.id,
      LEFT JOIN generic_files gf ON pe.generic_file_id = gf.id;
 
 -- Now mark the migration as completed.
-update schema_migrations set finished_at = now() where "version" = '018_old_uuid_removal';
+update schema_migrations set finished_at = now() where "version" = '015_old_uuid_removal';
